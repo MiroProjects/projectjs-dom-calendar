d4eecd9b508e4e48d833375d139cfae15b314bfe
Miroslav <miroslav.terziyski@outlook.com>
2019-02-14 13:41:30 +0200

    Added comments

--- jscalendar-script.js
+++ jscalendar-script.js
@@ -1,40 +1,53 @@
+//Global arrays for accessing the months and days of week
 var monthNames = ["January", "February", "March", "April", "May", "June",
 "July", "August", "September", "October", "November", "December"];
 var daysOfWeek = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+
+//Global variables for the current month and year
 var dateObject = new Date();
 var currentMonth = dateObject.getMonth();
 var currentYear = dateObject.getFullYear();
+
+//Constant variable for holding the element to which the callendar is atteched
 const elementToAppendToID = "js-calendar";
 
+//Function for getting the name of the month using the global array
 var getNameOfFirstDayOfMonth = function(year, month){
     var date = new Date(year, month, 1).toDateString();
     var nameOfDay = date.substr(0, 3);
     return nameOfDay;
 };
 
+//Function for getting the last date(1-31) of the month
 var getNumberOfLastDayOfMonth = function(year, month){
     var date =  new Date(year, month + 1, 0).toDateString();
     var lastDayOfMonth = date.substr(8, 2);
     return lastDayOfMonth;
 };
 
+//Main parameterized function for displaying the calendar
 var createCalendar = function(year, month){
+    //Firstly delete the previous calender 
     var child = JSDomL.getChilds(elementToAppendToID)[0];
     JSDomL.deleteElement(child);
 
+    //Set the current year and month to be the parameters values
     currentYear = year;
     currentMonth = month;
 
+    //Create div element for holding the title(displaying the arrows, date and the select field) and the table with the dates
     var element = JSDomL.appendElement("DIV", elementToAppendToID);
     addFunctionality(year, month, element);
     JSDomL.addAttribute(element, "id", "main-div")
           .addAttribute(element, "class", "container-div");
 
+    //Creating for the calendar
     var table = JSDomL.appendElement("TABLE", element);
     showDaysOfWeek(table);
     printDaysInMonth(year, month, table);
 }
 
+//Function for displaying the days of week (Mon-Sun) in the table using the global array
 var showDaysOfWeek = function(table){
     var tableRow = JSDomL.appendElement("tr", table);
     for (let index = 0; index < daysOfWeek.length; index++) {
@@ -44,6 +57,7 @@ var showDaysOfWeek = function(table){
     }
 };
 
+//Main function for printing all the days for the current year and month and the table parameter is for where to attach these elements
 var printDaysInMonth = function(year, month, table){
     var counter = 1;
     var firstDayName = getNameOfFirstDayOfMonth(year, month);
@@ -67,6 +81,8 @@ var printDaysInMonth = function(year, month, table){
         }
 
         for (let j = 0; j < 7; j++) {
+            //If the value of the counter is larger number then the last day of the month then break
+            //All of the days of the month are already displayed
             if (counter > lastDay) {
                 break;
             }
@@ -77,6 +93,7 @@ var printDaysInMonth = function(year, month, table){
     }
 };
 
+//Function for recreating the calendar for diplaying the prevoius month
 var previousMonth = function(){
     if (currentMonth == 0) {
         currentYear--;
@@ -89,6 +106,7 @@ var previousMonth = function(){
     createCalendar(currentYear, currentMonth);
 };
 
+//Function for recreating the calendar for diplaying the next month
 var nextMonth = function(){
     if (currentMonth == 11) {
         currentYear++;
@@ -100,6 +118,9 @@ var nextMonth = function(){
     createCalendar(currentYear, currentMonth);
 };
 
+//Function for creating the left and right arrow for the months
+//For creating the text for the date
+//For creating the select filed with all the options
 var addFunctionality = function(year, month, element){
     var div = JSDomL.appendElement("DIV", element);
     JSDomL.addAttribute(div, "class", "label");
@@ -132,4 +153,7 @@ var addFunctionality = function(year, month, element){
     }
 };
 
+//On load of the page call the function createCalendar with the current year and month
+//For test to show good parametrization change the current year and month to whatever you like: everything will
+//change based on the given information
 window.onload = createCalendar(currentYear, currentMonth);
